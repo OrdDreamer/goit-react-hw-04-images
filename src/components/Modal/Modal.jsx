68ee2a113086eb66021ee457ac48b72c11ac854a
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2023-03-11 12:57:46 +0200

    Use react hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,24 +1,24 @@
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import styles from './modal.module.css';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ close, children }) => {
+
+  const unplugModal = useCallback(({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === 'Escape') {
+      close();
+    }
+  }, [close]);
 
   useEffect(() => {
     document.addEventListener('keydown', unplugModal);
     return () => {
       document.removeEventListener('keydown', unplugModal);
     };
-  }, []);
-
-  const unplugModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      close();
-    }
-  };
+  }, [unplugModal]);
 
   return createPortal(
     <div className={styles.overlay} onClick={unplugModal}>
